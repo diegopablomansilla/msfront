90d93cda226dc81957b3e0f70865d5efbf0521cf
Diego Pablo Mansilla <diegopablomansilla@gmail.com>
2020-05-30 15:10:56 -0300

    CuentaContable 0.0.0.5

--- src/main/java/com/ms/front/view/cuenta_contable/CuentaContableTable.java
+++ src/main/java/com/ms/front/view/cuenta_contable/CuentaContableTable.java
@@ -2,17 +2,34 @@ package com.ms.front.view.cuenta_contable;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
+import com.ms.front.model.Pagin;
+import com.ms.front.services.PaginArgs;
+import com.ms.front.services.Service;
 import com.ms.front.services.ServiceArgs;
+import com.ms.front.view.JavaFXUtil;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
@@ -20,17 +37,45 @@ import javafx.scene.control.Tooltip;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class CuentaContableTable extends Controller {
+public class CuentaContableTable implements Initializable {
+
+	private static boolean MODE_SELECCIONAR = true;
+	private static boolean MODE_NORMAL = false;
 
 	private static final String TITLE = "Plan de cuentas";
 
+	private Stage stage;
+	private SimpleBooleanProperty modoSeleccionarProperty = new SimpleBooleanProperty();
+	private PaginArgs paginArgs = new PaginArgs();
 	private CuentaContablePaginArgs args;
 	private CuentaContablePaginArgs argsOld;
+	private Pagin pagin;
 
 	// =============================================================================================
+
+	@FXML
+	private AnchorPane view;
+
+	@FXML
+	private Button agregar;
+
+	@FXML
+	private Button cambiar;
+
+	@FXML
+	private Button eliminar;
+
+	@FXML
+	private Button copiar;
+
+	@FXML
+	private Button seleccionar;
 
 	@FXML
 	private ToggleGroup porToogleGroup;
@@ -51,6 +96,9 @@ public class CuentaContableTable extends Controller {
 	private ToggleButton porPuntoDeEquilibrio;
 
 	@FXML
+	private TableView<CuentaContableTableItem> table;
+
+	@FXML
 	private TableColumn<CuentaContableTableItem, String> codigo;
 
 	@FXML
@@ -67,6 +115,21 @@ public class CuentaContableTable extends Controller {
 
 	@FXML
 	private TextField filtro;
+
+	@FXML
+	private Label status;
+
+	@FXML
+	private Button buscar;
+
+	@FXML
+	private Label totalItems;
+
+	@FXML
+	private Label totalPages;
+
+	@FXML
+	private HBox pagination;
 
 	@FXML
 	private ChoiceBox<String> operator;
@@ -122,6 +185,32 @@ public class CuentaContableTable extends Controller {
 	}
 
 	@FXML
+	private void onKeyPressedTable(KeyEvent event) {
+
+		int index = table.getSelectionModel().getSelectedIndex();
+		int size = table.getItems().size();
+
+		if (event.getCode().equals(KeyCode.DOWN) && index > -1 && size > 0 && index == (size - 1)) {
+			onBuscarNext();
+		} else if (event.getCode().equals(KeyCode.UP) && index == 0) {
+			onBuscarBack();
+		} else if (event.getCode().equals(KeyCode.PAGE_DOWN)) {
+			onBuscarNext();
+		} else if (event.getCode().equals(KeyCode.PAGE_UP)) {
+			onBuscarBack();
+		} else if (event.getCode().equals(KeyCode.HOME)) {
+			onBuscarFirst();
+		} else if (event.getCode().equals(KeyCode.END)) {
+			onBuscarLast();
+		} else if (event.getCode().equals(KeyCode.DELETE)) {
+			onEliminar(null);
+		} else if (event.getCode().equals(KeyCode.INSERT)) {
+			onAgregar(null);
+		}
+
+	}
+
+	@FXML
 	private void onKeyTypedTable(KeyEvent event) {
 
 		int key = (int) event.getCharacter().charAt(0);
@@ -155,7 +244,9 @@ public class CuentaContableTable extends Controller {
 	private void onKeyTypedFiltro(KeyEvent event) {
 
 		int key = (int) event.getCharacter().charAt(0);
-		if (key == 13) {
+		System.out.println(event.isControlDown() + "  " + key) ;
+//		if (event.isControlDown() && key == 13) {
+		if (event.isControlDown() && key == 10) {
 			onBuscarStart();
 		}
 
@@ -164,10 +255,98 @@ public class CuentaContableTable extends Controller {
 	// =============================================================================================
 
 	@FXML
+	private void onAgregar(ActionEvent event) {
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle("Cuenta contable");
+		window.setWidth(300);
+		window.setHeight(200);
+		window.setMaxWidth(300);
+		window.setMaxHeight(200);
+		window.show();
+	}
+
+	@FXML
+	private void onCambiar(ActionEvent event) {
+		if (table.getSelectionModel().getSelectedIndex() > -1) {
+			Stage window = new Stage();
+			window.initModality(Modality.APPLICATION_MODAL);
+			window.setTitle(table.getSelectionModel().getSelectedItem().getCodigo());
+			window.setWidth(300);
+			window.setHeight(200);
+			window.setMaxWidth(300);
+			window.setMaxHeight(200);
+			window.show();
+		}
+	}
+
+	@FXML
+	private void onCopiar(ActionEvent event) {
+		if (table.getSelectionModel().getSelectedIndex() > -1) {
+			Stage window = new Stage();
+			window.initModality(Modality.APPLICATION_MODAL);
+			window.setTitle(table.getSelectionModel().getSelectedItem().getCodigo());
+			window.setWidth(300);
+			window.setHeight(200);
+			window.setMaxWidth(300);
+			window.setMaxHeight(200);
+			window.show();
+		}
+	}
+
+	@FXML
+	private void onEliminar(ActionEvent event) {
+		if (table.getSelectionModel().getSelectedIndex() > -1) {
+
+			Alert alertDeleteItem = JavaFXUtil
+					.buildAlertConfirmDeleteItem(table.getSelectionModel().getSelectedItem().toString());
+
+			Optional<ButtonType> result = alertDeleteItem.showAndWait();
+			table.requestFocus();
+			if ((result.isPresent())) {
+				if ((result.get() == ButtonType.OK)) {
+					System.out.println("ELIMINAR " + table.getSelectionModel().getSelectedItem());
+				}
+			}
+
+		}
+	}
+
+	@FXML
+	private void onSeleccionar(ActionEvent event) {
+		stage.close();
+	}
+
+	@FXML
+	private void onBuscar(ActionEvent event) {
+		onBuscarStart();
+	}
+
+	@FXML
 	private void onPorCuentaContable(ActionEvent event) {
 		args.setPorCuentaContable();
 		filtro.setPromptText("Buscar por cuenta contable");
 		onBuscarStart();
+	}
+
+	@FXML
+	void onActionBack(ActionEvent event) {
+		onBuscarBack();
+	}
+
+	@FXML
+	void onActionFirst(ActionEvent event) {
+		onBuscarFirst();
+	}
+
+	@FXML
+	void onActionLast(ActionEvent event) {
+		onBuscarLast();
+	}
+
+	@FXML
+	void onActionNext(ActionEvent event) {
+		onBuscarNext();
 	}
 
 	@FXML
@@ -198,6 +377,8 @@ public class CuentaContableTable extends Controller {
 		onBuscarStart();
 	}
 
+	// ================================================================================================
+
 	private void onBuscarChangeOperadorFiltro(int index) {
 
 		if (index == 0) {
@@ -210,156 +391,170 @@ public class CuentaContableTable extends Controller {
 		onBuscarStart();
 	}
 
-	// =============================================================================================
-	// =============================================================================================
-	// =============================================================================================
+	private void onBuscarStart() {
 
-	protected ServiceArgs getArgs() {
-		return args;
-	}
-//	
-//	protected void setArgs(ServiceArgs args) {
-//		this.args = (CuentaContablePaginArgs) args; 
-//	}
-//	
-//	protected ServiceArgs getArgsOld() {
-//		return argsOld;
-//	}
-//	
-//	protected void setArgsOld(ServiceArgs argsOld) {
-//		this.argsOld = (CuentaContablePaginArgs) argsOld;
-//	} 
-
-	protected boolean isArgsEqualsArgsOld() {
-		return this.argsOld != null && argsOld.equals(this.args);
-	}
-
-	protected void toAssignArgsToArgsOld() {
-		argsOld = this.args.clone();
-	}
-
-	protected String getSelectedId() {
-		if (table.getSelectionModel().getSelectedIndex() > -1) {
-			return table.getSelectionModel().getSelectedItem().getId();
+		if (argsOld != null && argsOld.equals(args)) {
+			return;
 		}
 
-		return null;
+		onBuscarFirst();
+
+		argsOld = args.clone();
 	}
 
-	protected boolean selectId(String id) {
-		for (int i = 0; i < table.getItems().size(); i++) {
-			if (table.getItems().get(i).getId().equals(id)) {
-				table.getSelectionModel().select(i);
-				return true;
+	private void onBuscarNext() {
+		paginArgs.setPageRequestNext();
+		onBuscar("Buscando siguiente página...");
+	}
+
+	private void onBuscarBack() {
+		paginArgs.setPageRequestBack();
+		onBuscar("Buscando página anterior...");
+	}
+
+	private void onBuscarFirst() {
+		paginArgs.setPageRequestFirst();
+		paginArgs.setLastIndexOld(0);
+		onBuscar("Buscando la primer página...");
+//		argsOld = args.clone();
+	}
+
+	private void onBuscarLast() {
+		paginArgs.setPageRequestLast();
+		paginArgs.setLastIndexOld(0);
+		onBuscar("Buscando última página...");
+	}
+
+	private void onBuscar(String msg) {
+
+		status.setText(msg);
+
+		String lastId = null;
+		if (table.getSelectionModel().getSelectedIndex() > -1) {
+			lastId = table.getSelectionModel().getSelectedItem().getId();
+		}
+
+		List<CuentaContableTableItem> items = findAll();
+
+		table.getItems().clear();
+		table.getItems().addAll(items);
+		if (table.getItems().size() > 0) {
+//			table.getSelectionModel().select(0);
+			table.getSelectionModel().selectFirst();
+			for (int i = 0; i < table.getItems().size(); i++) {
+				if (table.getItems().get(i).getId().equals(lastId)) {
+					table.getSelectionModel().select(i);
+					break;
+				}
 			}
 		}
+		table.requestFocus();
 
-		return false;
+		status.setText("");
 	}
 
-	protected void findAllBefore() {
+	// ==========================================================================
+
+	private List<CuentaContableTableItem> findAll() {
+
 		if (args.getOperador() == null) {
 			args.setOperador(ServiceArgs.OP_SW_ICT_O);
 		}
-	}
 
-	protected String getFindAllUrl() {
-		return "CuentaContable/findAll";
-	}
+		totalItems.setText("");
+		totalPages.setText("");
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	protected void mapper(List items, Object[][] t) {
+		List<CuentaContableTableItem> items = new ArrayList<CuentaContableTableItem>();
 
-		for (int i = 0; i < t.length; i++) {
+		try {
 
-			CuentaContableTableItem item = new CuentaContableTableItem();
+			String urlString = "CuentaContable/findAll";
 
-			int j = 0;
-			if (t[i][j] != null) {
-				item.setId(t[i][j].toString());
+			pagin = Service.GETPagin(urlString, paginArgs, args);
+			paginArgs.setLastIndexOld(pagin.getLastIndex());
+
+			if (pagin.getThisPageItems() == null || pagin.getThisPageItems().length == 0) {
+				return items;
 			}
 
-			j++;
-			if (t[i][j] != null) {
-				item.setCodigo(t[i][j].toString());
+			totalItems.setText(pagin.getCantRows().toString() + " ítems");
+			totalPages.setText("Página " + (pagin.getThisPage().getPageNumber() + 1) + " de " + (pagin.getCantPages()));
+
+			Object[][] t = pagin.getThisPageItems();
+
+			for (int i = 0; i < t.length; i++) {
+
+				CuentaContableTableItem item = new CuentaContableTableItem();
+
+				int j = 0;
+				if (t[i][j] != null) {
+					item.setId(t[i][j].toString());
+				}
+
+				j++;
+				if (t[i][j] != null) {
+					item.setCodigo(t[i][j].toString());
+				}
+
+				j++;
+				if (t[i][j] != null) {
+					item.setNombre(t[i][j].toString());
+				}
+
+				j++;
+				if (t[i][j] != null) {
+					item.setCentroCostoContable(t[i][j].toString());
+				}
+
+				j++;
+				if (t[i][j] != null) {
+					item.setCuentaAgrupadora(t[i][j].toString());
+				}
+
+				j++;
+				if (t[i][j] != null) {
+					item.setPorcentaje(t[i][j].toString());
+				}
+
+				items.add(item);
 			}
 
-			j++;
-			if (t[i][j] != null) {
-				item.setNombre(t[i][j].toString());
-			}
+			return items;
 
-			j++;
-			if (t[i][j] != null) {
-				item.setCentroCostoContable(t[i][j].toString());
-			}
-
-			j++;
-			if (t[i][j] != null) {
-				item.setCuentaAgrupadora(t[i][j].toString());
-			}
-
-			j++;
-			if (t[i][j] != null) {
-				item.setPorcentaje(t[i][j].toString());
-			}
-
-			items.add(item);
+		} catch (Exception e) {
+			JavaFXUtil.buildAlertException(e);
 		}
+		return items;
 
-	}
-
-	// ================================================================================================
-
-	private void initializeAfter(CuentaContablePaginArgs args) {
-		this.args = args;
-		filtro.textProperty().bindBidirectional(args.filtroProperty());
-		onPorCuentaContable(null);
-	}
-
-	private static void initializeBefore(CuentaContablePaginArgs args) {
-		if (args.getEjercicioContable() == null) {
-			throw new IllegalArgumentException("args.getEjercicioContable() is null");
-		}
-	}
-
-	public static void show(Stage stage, Node owner, CuentaContablePaginArgs args) throws IOException {
-
-		initializeBefore(args);
-
-		CuentaContableTable controller = (CuentaContableTable) load(CuentaContableTable.class, TITLE, stage, owner,
-				MODE_NORMAL);
-
-		controller.initializeAfter(args);
-
-		stage.show();
-	}
-
-	public static CuentaContableTableItem showAndWait(Stage stage, Node owner, CuentaContablePaginArgs args)
-			throws IOException {
-
-		initializeBefore(args);
-
-		CuentaContableTable controller = (CuentaContableTable) load(CuentaContableTable.class, TITLE, stage, owner,
-				MODE_SELECCIONAR);
-
-		controller.initializeAfter(args);
-
-		stage.initModality(Modality.APPLICATION_MODAL);
-		stage.showAndWait();
-
-		if (controller.table.getSelectionModel().getSelectedIndex() > -1) {
-			return controller.table.getSelectionModel().getSelectedItem();
-		}
-
-		return null;
 	}
 
 	// ================================================================================================
 
 	public void initialize(URL url, ResourceBundle rb) {
 
-		super.initialize(url, rb);
+		agregar.setTooltip(new Tooltip("Agregar (ALT+A)"));
+		cambiar.setTooltip(new Tooltip("Cambiar (ALT+C)"));
+		eliminar.setTooltip(new Tooltip("Eliminar (ALT+E)"));
+		copiar.setTooltip(new Tooltip("Copiar (ALT+I)"));
+		seleccionar.setTooltip(new Tooltip("Seleccionar (ALT+S)"));
+		buscar.setTooltip(new Tooltip("Buscar (ALT+B)"));
+		table.setTooltip(new Tooltip("Buscar (CTRL+ENTER)"));
+
+		// --------------------------------------------------------------------------
+
+		seleccionar.visibleProperty().bindBidirectional(modoSeleccionarProperty);
+
+		// --------------------------------------------------------------------------
+
+		table.setItems(FXCollections.observableArrayList());
+
+		// --------------------------------------------------------------------------
+
+		cambiar.disableProperty().bind(Bindings.size(table.getItems()).isEqualTo(0));
+		eliminar.disableProperty().bind(Bindings.size(table.getItems()).isEqualTo(0));
+		copiar.disableProperty().bind(Bindings.size(table.getItems()).isEqualTo(0));
+		seleccionar.disableProperty().bind(Bindings.size(table.getItems()).isEqualTo(0));
+		pagination.disableProperty().bind(Bindings.size(table.getItems()).isEqualTo(0));
 
 		// --------------------------------------------------------------------------
 
@@ -380,6 +575,87 @@ public class CuentaContableTable extends Controller {
 		operator.getSelectionModel().selectedIndexProperty()
 				.addListener((obs, oldV, newV) -> onBuscarChangeOperadorFiltro((int) newV));
 
+	}
+
+	// ================================================================================================
+
+	
+
+	private static CuentaContableTable show(Stage stage, Node owner, boolean modoSeleccionar,
+			CuentaContablePaginArgs filter) throws IOException {
+
+		CuentaContableTable viewController = loadView(modoSeleccionar, filter);
+		viewController.stage = stage;
+
+		Scene scene = new Scene(viewController.view);
+
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent ke) {
+				if (ke.getCode() == KeyCode.ESCAPE) {
+					stage.close();
+				}
+			}
+		});
+
+		stage.setScene(scene);
+		stage.setTitle(TITLE);
+		stage.setAlwaysOnTop(false);
+		stage.setResizable(false);
+		stage.centerOnScreen();
+		stage.initOwner(owner.getScene().getWindow());
+
+		stage.initStyle(StageStyle.UTILITY);
+//		stage.initStyle(StageStyle.DECORATED);
+//		stage.initStyle(StageStyle.UNDECORATED);
+//		stage.initStyle(StageStyle.TRANSPARENT);
+
+		viewController.table.requestFocus();
+
+		if (modoSeleccionar) {
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.showAndWait();
+		} else {
+			stage.show();
+		}
+
+		return viewController;
+
+	}
+
+	public static CuentaContableTableItem showAndWait(Stage stage, Node owner, CuentaContablePaginArgs filter)
+			throws IOException {
+
+		CuentaContableTable viewController = show(stage, owner, MODE_SELECCIONAR, filter);
+
+		if (viewController.table.getSelectionModel().getSelectedIndex() > -1) {
+			return viewController.table.getSelectionModel().getSelectedItem();
+		}
+
+		return null;
+	}
+
+	public static void show(Stage stage, Node owner, CuentaContablePaginArgs filter) throws IOException {
+		show(stage, owner, MODE_NORMAL, filter);
+	}
+	
+	private static CuentaContableTable loadView(boolean modoSeleccionar, CuentaContablePaginArgs filter)
+			throws IOException {
+
+		if (filter.getEjercicioContable() == null) {
+			throw new IllegalArgumentException("filter.getEjercicioContable() is null");
+		}
+
+		FXMLLoader loader = new FXMLLoader(CuentaContableTable.class.getResource("CuentaContableTable.fxml"));
+
+		loader.load();
+
+		CuentaContableTable viewController = loader.getController();
+		viewController.modoSeleccionarProperty.set(modoSeleccionar);
+		viewController.args = filter;
+		viewController.filtro.textProperty().bindBidirectional(filter.filtroProperty());
+		viewController.onPorCuentaContable(null);
+
+		return viewController;
 	}
 
 }
